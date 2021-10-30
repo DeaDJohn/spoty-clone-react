1607da03e4b53aea12e0a101575f7301be234f5c
Juanjo Tooltyp <juanjo@tooltyp.com>
2021-10-30 11:58:37 +0200

    Bug name in function

--- src/Footer.js
+++ src/Footer.js
@@ -29,7 +29,7 @@ function Footer() {
     getCurrentSong();
   }
   function skipPrevSong() {
-    spotify.skipToPrev();
+    spotify.skipToPrevious();
     getCurrentSong();
   }
   function pauseSong(){
