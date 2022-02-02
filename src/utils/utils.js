2dc2e867f4f28d4fd3440a5c413854a825b573c5
juanjo <juanjofb86@gmail.com>
2022-02-02 23:07:09 +0100

    Try to play the song that is pressed

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -21,20 +21,41 @@ export const getArtistAlbums = async (id, token) => {
       }
 }
 export const setSongQueue = async (uri, token) => {
-  
-  const albumUrl = `https://api.spotify.com/v1/me/player/queue?uri=${uri}`;
-  const response = await fetch(albumUrl, 
+  const devices = await getDevice(token);
+  console.log(devices);
+  const queueUrl = `https://api.spotify.com/v1/me/player/queue?uri=${uri}&device_id=${devices}`;
+  const response = await fetch(queueUrl,
     {method: 'POST',
     headers: {
-    
+
       "Accept": "application/json",
       "Content-Type": "application/json",
       "Authorization": `Bearer ${token}`,
     }});
 
-    console.log(response);
     if (response.status == 204) {
-      return response.json();
+      const res = await response.json();
+      console.log( res);
+      return res;
+    } else {
+      throw new Error(response.status);
+    }
+}
+
+export const getDevice = async (token) => {
+  const deviceUrl = `https://api.spotify.com/v1/me/player/devices`;
+  const response = await fetch(deviceUrl,
+    {headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`,
+    }});
+
+    if (response.status == 200) {
+      const devices = await response.json();
+      const device = devices.devices.filter(item =>  item.is_active == true )
+
+      return device[0].id;
     } else {
       throw new Error(response.status);
     }
