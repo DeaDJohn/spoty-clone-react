e72a8d20537a39afbb58eb4b68696fe904bbac3e
Juanjo Fernandez <juanjofb86@gmail.com>
2022-02-03 20:16:28 +0100

    Add add to play queue functionality

--- src/items/SongTop.js
+++ src/items/SongTop.js
@@ -1,20 +1,54 @@
 import React from "react";
 // import {Link} from 'react-router-dom';
-import {millisecondsToMinutesAndSeconds, setSongQueue} from '../utils/utils';
+import {millisecondsToMinutesAndSeconds, getDevice} from '../utils/utils';
 import { useDataLayerValue } from "../DataLayer";
+import { useState } from "react";
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+
 
 function SongTop({item, index}) {
 	console.log(useDataLayerValue());
 	const [{ token }] = useDataLayerValue();
 
+	const [addQueue, setAddQueue] = useState(false);
+	const [addingQueue, setAddingQueue] = useState(false);
+
+	const setSongQueue = async (uri, token) => {
+		setAddingQueue(true);
+		const devices = await getDevice(token);
+		console.log(devices);
+		const queueUrl = `https://api.spotify.com/v1/me/player/queue?uri=${uri}&device_id=${devices}`;
+		const response = await fetch(queueUrl,
+			{method: 'POST',
+			headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${token}`,
+			}});
+
+			
+			if (response.status == 204) {
+				// const res = await response.json();
+				setTimeout(() => {
+					setAddQueue(false);
+					setAddingQueue(false);
+				}, 1500);
+				setAddQueue(true);
+				return true;
+			} else {
+				throw new Error(response.status);
+			}
+	}
+
   return (
-	<div id={`songTop-${item?.id}`} className="section-topSong_item"  className="songTop" onClick={() => setSongQueue(item?.uri, token)}>
+	<div id={`songTop-${item?.id}`}  className={`songTop ${addingQueue ? "loading" : ""}`} onClick={() => setSongQueue(item?.uri, token)}>
 		<div className="songTop_row">
 			<div className="songTop_position">
 				<span>{index}</span>
 			</div>
 			<div className="songTop_information">
 				{item?.album?.images[0].url && <img src={item?.album.images[0].url} alt={item?.name} className="songTop__album" />}
+				<AddCircleOutlineIcon fontSize="medium" className="added__icon" />
 				<div className="songTop__info">
 					<p>{item?.name}</p>
 				</div>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,62 +1,41 @@
 
 export function millisecondsToMinutesAndSeconds(millis) {
-    var minutes = Math.floor(millis / 60000);
-    var seconds = ((millis % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+		var minutes = Math.floor(millis / 60000);
+		var seconds = ((millis % 60000) / 1000).toFixed(0);
+		return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
 export const getArtistAlbums = async (id, token) => {
 
-    const albumUrl = `https://api.spotify.com/v1/albums/${id}`;
-    const response = await fetch(albumUrl, {headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`,
-      }});
+		const albumUrl = `https://api.spotify.com/v1/albums/${id}`;
+		const response = await fetch(albumUrl, {headers: {
+				"Accept": "application/json",
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${token}`,
+			}});
 
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        throw new Error(response.status);
-      }
-}
-export const setSongQueue = async (uri, token) => {
-  const devices = await getDevice(token);
-  console.log(devices);
-  const queueUrl = `https://api.spotify.com/v1/me/player/queue?uri=${uri}&device_id=${devices}`;
-  const response = await fetch(queueUrl,
-    {method: 'POST',
-    headers: {
-
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`,
-    }});
-
-    if (response.status == 204) {
-      const res = await response.json();
-      console.log( res);
-      return res;
-    } else {
-      throw new Error(response.status);
-    }
+			if (response.status == 200) {
+				return response.json();
+			} else {
+				throw new Error(response.status);
+			}
 }
 
 export const getDevice = async (token) => {
-  const deviceUrl = `https://api.spotify.com/v1/me/player/devices`;
-  const response = await fetch(deviceUrl,
-    {headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`,
-    }});
+	const deviceUrl = `https://api.spotify.com/v1/me/player/devices`;
+	const response = await fetch(deviceUrl,
+		{headers: {
+			"Accept": "application/json",
+			"Content-Type": "application/json",
+			"Authorization": `Bearer ${token}`,
+		}});
 
-    if (response.status == 200) {
-      const devices = await response.json();
-      const device = devices.devices.filter(item =>  item.is_active == true )
+		if (response.status == 200) {
+			const devices = await response.json();
+			const device = devices.devices.find(item =>  item.is_active == true )
 
-      return device[0].id;
-    } else {
-      throw new Error(response.status);
-    }
+			return device.id;
+		} else {
+			throw new Error(response.status);
+		}
 }
